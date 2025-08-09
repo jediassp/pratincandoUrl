329eee0e23779d6f1dd936d2f04419cb8d01f048
Jefferson <112223730+jediassp@users.noreply.github.com>
2025-08-09 13:56:13 -0300

    desafio 1001
    Codigo desafio e descrição

---
+++ Iniciante/1000.py
@@ -0,0 +1,7 @@
+'''
+Desafio 1001, nome Hello World!.
+O primeiro desafio é imprimir uma mensagem 
+
+ >Você deve imprimir a mensagem "Hello World!".
+'''
+print('Hello World!')#saida:Hello World!
---
+++ Iniciante/1001.py
@@ -0,0 +1,13 @@
+'''
+Desafio 1001, nome extremente básico.
+>resumo receber dois valores, armazenar em uma variavel (A e B), soma-los e seguida imprimi-los.
+>primeiro atraves do metodo de entrada de dados receber, converter em inteiro e armazenar na variavel A e B.
+>segundo soma-los e armazena-los na variavel X em letra maiuscula 
+>terceiro imprimir o resultado conforme o exemplo dado 
+"X = " 
+com os espaços entre apos o X e apos =, na impressão.
+'''
+A = int(input())#entrada:10
+B = int(input())#entrada:9
+X = A + B
+print('X =',X)#saida:X = 19
