83cd35cf03ab845d6b588f746820713b7fd3e593
Jefferson <112223730+jediassp@users.noreply.github.com>
2025-08-09 13:55:27 -0300

    Delete 1000.py

--- desafios/1000.py
+++
@@ -1,6 +0,0 @@
-'''
-O primeiro desafio é imprimir uma mensagem 
-
- >Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
-'''
-print('Hello World!')#saida:Hello World!
