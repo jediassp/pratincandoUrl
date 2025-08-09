5a276d57cc0f7713823a1332ee8c61b063f511b3
Jefferson <112223730+jediassp@users.noreply.github.com>
2025-08-09 13:14:30 -0300

    Desafio
    Desafio a ser feito e o Chamado do desafio explicando.

--- desafios/1000.py
+++ desafios/1000.py
@@ -1,1 +1,6 @@
+'''
+O primeiro desafio é imprimir uma mensagem 
+
+ >Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo.
+'''
 print('Hello World!')
