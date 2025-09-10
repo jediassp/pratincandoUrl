58e390e2e65cc0c7bfb2a5c0850643fae8aef931
Jefferson <112223730+jediassp@users.noreply.github.com>
2025-09-10 15:43:38 -0300

    Desafio 1004.py
    Código,  descrição e passo a passo para realizar o desafio.

---
+++ Iniciante/1004.py
@@ -0,0 +1,11 @@
+'''
+Desafio 1004 ler dois valores inteiros e efetuar o calculo do produto, atribuir o resultado a uma variavel PROD e após mostrar essa variavel.
+> 1 entrada de dois valores inteiros
+> 2 criar uma variavel que recebe o valor do calculo 
+> 3 imprimir a variavel com a mensagem 'PROD = ' de acordo com o solicitado.
+'''
+
+num1 = int(input(''))#entrada 
+num2 = int(input(''))#entrada
+PROD = num1 * num2#calculo
+print('PROD = {}'.format(PROD))#output, mensagem
